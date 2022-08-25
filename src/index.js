1730db3871cbc19c7813bc2d878ba4ae94b401e6
Waseem <mw11221122@gmail.com>
2022-08-25 17:06:16 +0500

    Css file is added in react folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App(){
+  return (<><div className='container'>
+          <div className='header'>
+            <div className='logo'>Wtech</div>
+            <div className='nav'>
+              <ul>
+                <li><a>Home</a></li>
+                <li><a>About Us</a></li>
+                <li><a>Services</a></li>
+                <li><a>Blogs</a></li>
+                <li><a>Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </div></>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
