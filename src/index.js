ec28ab0d52e52466eefc0cf6873b468744f37d93
Waseem <mw11221122@gmail.com>
2022-08-27 04:07:00 +0500

    This is simple landing page in react.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function Content(){
+  return (<><div className='content'>
+          <h1>Wtech And App Development</h1>
+          <p>This is Web and App Development plateform</p>
+          <button>Watch Free Videos</button>
+          <button>Download Apps</button>
+          </div></>)
+}
+
 function App(){
   return (<><div className='container'>
           <div className='header'>
             <div className='logo'>Wtech</div>
             <div className='nav'>
               <ul>
-                <li><a>Home</a></li>
-                <li><a>About Us</a></li>
-                <li><a>Services</a></li>
-                <li><a>Blogs</a></li>
-                <li><a>Contact Us</a></li>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About Us</a></li>
+                <li><a href='#'>Services</a></li>
+                <li><a href='#'>Blogs</a></li>
+                <li><a href='#'>Contact Us</a></li>
               </ul>
             </div>
           </div>
+          <Content/>
 
         </div></>);
 }
